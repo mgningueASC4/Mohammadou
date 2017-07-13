7e8098e0926def8d52e3a50b6381b0439d4bcae6
mgningueASC4 <gninguem@bxscienc.edu>
2017-07-13 09:46:58 -0400

    updated pokemon, 3rd bonus in progress

--- pokemonfunctions.js
+++ pokemonfunctions.js
@@ -1,6 +1,8 @@
 // array of alphabet 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
+
+
 // function which makes a random letter
 function randLetter(){
     return alphabet[Math.floor(Math.random()*alphabet.length)]//gets alphabet letter from random number that is the length of the alphabet
@@ -20,11 +22,22 @@ function randSentence(){
     var sentence = ""
     var phraselength = Math.floor(Math.random()*15)+2
     for (i=0; i<=phraselength; i++){
+        if (i == 0){
+
+        }
         sentence += " ";
         sentence += randWord();
     }
     sentence += "."
-    return sentence
+    return (sentence.charAt(1).toUpperCase() + sentence.slice(2))
+}
+console.log(randSentence());
+
+var firstPokemon = ["Vulpix", 41, 38]
+var secondPokemon = ["Arcanine", 110, 90]
+function pokeAttack(firstPokemon, secondPokemon){
+    secondPokemon[2] = (secondPokemon[2]- firstPokemon[1]);
+    return secondPokemon;
 }
 
-console.log(randSentence());
+console.log(pokeAttack(firstPokemon, secondPokemon));
