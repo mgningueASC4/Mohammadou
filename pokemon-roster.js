5d475667583e3a8b8b2c2c7ee78f818a7531aa7f
mgningueASC4 <gninguem@bxscienc.edu>
2017-07-13 16:08:54 -0400

    updated roster

--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,11 +1,8 @@
-var pokemonRoster = [["Ninetales", "Abra", "Alakazam" ]
-["Fire", "Psychic", "Psychic"]
-[73, 25, 55]
-[76, 20, 50 ]
-[75, 15, 45]
-[false, false, true]]
+var pokemonRoster = [["Ninetales", "Fire", 73, 76, 75, false] 
+["Abra", "Psychic", 25, 20, 15, false]
+["Alakazam", "Psychic", 55, 50, 45, true]
+["Pikachu", "Electric", 35, 55, 40, false]]
 
-for (var i = 0; i <= 5, i++;){
-    console.log(i + ". Name: " pokemonRoster[0][i] + " > Type: "+ pokemonRoster[1][i] + " > HP: " + pokemonRoster[2][i] +" > Attack: "+ pokemonRoster[3][i] + " > Defense: "+ pokemonRoster[4][i] + " > Legend: " pokemonRoster[5][i]
-    )
+for (var i = 0; i <= pokemonRoster.length, i++;){
+    console.log(i + ". Name: " pokemonRoster[i][0] + " > Type: "+ pokemonRoster[i][1] + " > HP: " + pokemonRoster[i][2] +" > Attack: "+ pokemonRoster[i][3] + " > Defense: "+ /*Ci goes over the number*/ pokemonRoster[i][4] + " > Legend: " pokemonRoster[i][5])
 }
