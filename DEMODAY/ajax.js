979a1cf2f48bf2038e3d1db6b587419a9bf7fd2f
mgningueASC4 <gninguem@bxscienc.edu>
2017-08-16 14:31:38 -0400

    All updated, better-functioning cart

--- DEMODAY/ajax.js
+++ DEMODAY/ajax.js
@@ -1,16 +1,28 @@
 // add to cart function
 var shoppingCart =[];
-function addtocart(product,money){
-    shoppingCart.push(product)
-    console.log(shoppingCart);
-    $("#list").append("<li>" +product+" $"+money+"</li>")
-}
+let totalprice =0;
+function addtocart(product,money,image){
+    shoppingCart.push(product);
+ // new console.log
+    console.log("+image "+image);
+ // new #shoplist
+    $("#shoplist").append(`<tr><td><img class ='pimage' src ='${image}'</td><td>${product}</td><td>$${money}</td></tr>`);
+  // total price stuff (all 3 lines)
+    totalprice+=money;
+   console.log(totalprice);
+   $('.totalprice').empty().append(`
+    <h3> Total: ${totalprice}</h3>`);
+}   
 
 function setup(){
   
     $("#searching").submit(function(e){
         e.preventDefault();
-        search()
+        search();
+    })
+    $('#checkout').click(function(e) {
+        alert("Thank you for your purchase!");
+        $('#shoplist').empty();
     })
 
 }
@@ -20,7 +32,7 @@ function search(){
     var search = $("#searchbar").val();
     console.log(search);
     productName=$("#searchbar").val();
-    prodName = encodeURI(productName)
+    prodName = encodeURI(productName);
     console.log(prodName);
     const apiKey = "TLHyGyOHvYVRxIeEgubFti0thQPGSMLHeeU";
     $.getJSON(
@@ -30,7 +42,7 @@ function search(){
             console.log(info);
           for (var val of info ){
             let lowest_price = val.product_lowest_price;
-            // console.log(lowest_price);
+            console.log(lowest_price);
             // if (val.product_category === "mobile" && val.product_sub_category ==="mobile"){
             let id = val.product_id//gets id used in second url for getting store info
             $.getJSON(
@@ -46,27 +58,56 @@ function search(){
                 for (var store of retailers){//loops through all stores
                   for (nom in store){
                     if(Object.keys(store[nom]).length>0){//checks if sold by that retailer (if retailer info is present in the array)
-                    // console.log(store);
-                     let price = store[nom].product_price;//price of each object
+                    // WHERE TO ADD IF UNDER SAME PROD_ID, WANT TO BE IN ONE BOX, NOT MANY SEPARATE PROD, THEN FIND CHEAPEST)
+                      // console.log(store);
+                      console.log(lowest_price);
+                console.log(typeof(lowest_price));
+      
+                    let logo = store[nom].product_store_logo;
+                    let atStore = store[nom].product_store_url 
+                     let price = Number(store[nom].product_price);//price of each object
                      console.log(price);
-                    //  if(lowest_price == price){
+                     console.log(typeof(price));
+                     console.log(lowest_price === price);
+                     if(lowest_price === price){
                         $('.Products').append(
                             `<section>
                             <div class = "card">
+                              <p><span class="new badge" data-badge-caption="LOWEST PRICE"></span></p>
                               <div class="card-image">
                                 <img class = "prod_img" src="${imag}">
                               </div>
                               <div class="card-content">
                                 <p><span class="card-title black-text">${nem}</span></p>
                                 <p><span class="card-title black-text">${nom}</span></p>
+                                <a href=${atStore}><img src=${logo} class="logo"></a>
                                 <p>₹${price}</p>
-                                 <p><i class="material-icons">add</i>n</p> 
-                                 <a onclick="addtocart('${nem}',200)" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>add to cart</a>
+      
+                                 <p><i class="material-icons">add</i></p> 
+                                 <a onclick="addtocart('${nem}',${price},'${imag}')" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>ADD TO CART</a>
                               </div>
                             </div>
                             </section>`
                           )
-                      // }
+                      }
+                      else{
+                        $('.Products').append(
+                            `<div class = "card">
+                              <div class="card-image">
+                                <img class = "prod_img" src="${imag}">
+                              </div>
+                              <div class="card-content">
+                                <p><span class="card-title black-text">${nem}</span></p>
+                                <p><span class="card-title black-text">${nom}</span></p>
+                                <a href=${atStore}><img src=${logo} class="logo"></a>
+                                <p>₹${price}</p>
+                                 <p><i class="material-icons">add</i></p> 
+                                 <a onclick="addtocart('${nem}',${price},'${imag}')" class="waves-effect waves-light btn"><i class="material-icons left">add_shopping_cart</i>add to cart</a>
+                              </div>
+                            </div>
+                            </section>`
+                          )
+                      }
                     }
                   }
                 }
@@ -80,6 +121,9 @@ function search(){
         }
       }
     )
+  function checkout(){
+
+  }
 }
 
 $(document).ready(setup);
